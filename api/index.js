fa74a97279dc1b1e12e5fa37bd5c8615a53eea40
jamc91 <albertom09@icloud..com>
2023-08-08 20:27:26 -0700

    remove vercel.json

--- api/index.js
+++ api/index.js
@@ -14,53 +14,53 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 routerApi(app)
 await mongoClient.connect()
 
-// app.get('/uploadPromotions', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../index.html'))
-// })
+app.get('/uploadPromotions', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'))
+})
 
-// app.get('/uploadAditionalPromotions', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../adPromotion.html'))
-// })
+app.get('/uploadAditionalPromotions', (req, res) => {
+  res.sendFile(path.join(__dirname, '../adPromotion.html'))
+})
 
-// app.get('/uploadPrices', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../variants.html'))
-// })
+app.get('/uploadPrices', (req, res) => {
+  res.sendFile(path.join(__dirname, '../variants.html'))
+})
 
-// app.post('/upload', upload.single('excel'), async (req, res, next) => {
-//   try {
-//     const file = req.file.path
-//     const json = parseXlsxToJSON(file)
-//     Promotion.collection.drop()
-//     await Promotion.insertMany(json)
+app.post('/upload', upload.single('excel'), async (req, res, next) => {
+  try {
+    const file = req.file.path
+    const json = parseXlsxToJSON(file)
+    Promotion.collection.drop()
+    await Promotion.insertMany(json)
 
-//     res.status(200).json({ message: 'Promociones actualizadas' })
-//   } catch (error) {
-//     console.log(error.message)
-//     res.status(500).json({ message: 'ha ocurrido un error.' })
-//   }
-// })
+    res.status(200).json({ message: 'Promociones actualizadas' })
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({ message: 'ha ocurrido un error.' })
+  }
+})
 
-// app.post('/uploadAditionalsPromotions', upload.single('excel-adPromotions'), async (req, res, next) => {
-//   try {
-//     const file = req.file.path
-//     const json = parseXlsxToJSON(file)
-//     // const promotions = await Promotion.find({})
+app.post('/uploadAditionalsPromotions', upload.single('excel-adPromotions'), async (req, res, next) => {
+  try {
+    const file = req.file.path
+    const json = parseXlsxToJSON(file)
+    // const promotions = await Promotion.find({})
 
-//     const operations = json.map(promotion => {
-//       return {
-//         updateOne: {
-//           filter: { key: promotion.key },
-//           update: { $push: { plans: { $each: promotion.plans, $position: 0 } } }
-//         }
-//       }
-//     })
-//     await Promotion.bulkWrite(operations)
+    const operations = json.map(promotion => {
+      return {
+        updateOne: {
+          filter: { key: promotion.key },
+          update: { $push: { plans: { $each: promotion.plans, $position: 0 } } }
+        }
+      }
+    })
+    await Promotion.bulkWrite(operations)
 
-//     res.status(200).json({ message: 'Promociones actualizadas' })
-//   } catch (error) {
-//     console.log(error.message)
-//     res.status(500).json({ message: 'ha ocurrido un error.' })
-//   }
-// })
+    res.status(200).json({ message: 'Promociones actualizadas' })
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({ message: 'ha ocurrido un error.' })
+  }
+})
 
 app.listen(PORT, console.log(`Ready on: http://localhost:${PORT}`))
