0d800d477dba7bc8fc1d7ec7ad1e98fee5a4e21f
jamc91 <albertom09@icloud..com>
2023-08-08 20:07:10 -0700

    vercel.json changes 2

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors({ origin: '*' }))
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 export default app
